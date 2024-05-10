84087377c9ab7754bc6f230216fbe14d4926c3ea
Uday kumar <udayjothula@gmail.com>
2024-05-10 12:45:31 +0000

    Updated main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "storage"{
-    source="./modules/storage"
+    source="./storage"
     name                      = var.storage_account_name
     resource_group_name       = var.resource_group_name
     location                  = var.location
