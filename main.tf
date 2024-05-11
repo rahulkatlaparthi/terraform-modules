c0f0a78c14ec2e92d19ca05da2cb542e117d25e8
Uday kumar <udayjothula@gmail.com>
2024-05-11 04:32:15 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ provider "azurerm" {
 module "storage"{
     source = "./terraform-azurerm-vnet"
     name                      = var.storage_account_name
-    resource_group_name       = var.resource_group_name
-    location                  = var.location
+    resource_group_name       = "example-resources"
+    location                  = "West Europe"
     access_tier= var.access_tier
-    account_kind              = var.account_kind
-    account_tier              = var.account_tier
-    account_replication_type  = var.account_replication_type
-    min_tls_version           = var.min_tls_version
-    tags                      = var.tags
+    account_kind              = "StorageV2"
+    account_tier              = "Cool"
+    account_replication_type  = "LRS"
+    min_tls_version           = "TLS1_2"
+
 }
