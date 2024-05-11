374ba98ae0aeeed22c35b45c2fd1b082901e51bc
Uday kumar <udayjothula@gmail.com>
2024-05-11 06:23:13 +0000

    Updated variables.tf

--- Storage/variables.tf
+++ Storage/variables.tf
@@ -1,1 +1,20 @@
-h
+// variable "create_resource_group" {
+//   description = "Whether to create resource group and use it for all networking resources"
+//   default     = false
+//   type        = bool
+// }
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+
+
