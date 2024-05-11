10bed9b6a139663bd0c0ea7f517f78110813ea75
Uday kumar <udayjothula@gmail.com>
2024-05-11 09:36:08 +0000

    Updated variables.tf

--- appserviceplan/variables.tf
+++ appserviceplan/variables.tf
@@ -1,1 +1,12 @@
-h
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
