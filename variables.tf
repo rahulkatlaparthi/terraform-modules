8731b96f573e3e0cff305584c0af75c5ea14a9c2
Uday kumar <udayjothula@gmail.com>
2024-05-11 06:04:37 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -6,12 +6,15 @@
 
 variable "resource_group_name" {
   description = "A container that holds related resources for an Azure solution"
+    default     = "example-resourcesss"
+  
 
   type        = string
 }
 
 variable "location" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+   default     = "West Europe"
 
   type        = string
 }
