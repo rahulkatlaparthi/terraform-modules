b47be1ef2c778f0b75828f8e7ef3df156d6e14ab
Uday kumar <udayjothula@gmail.com>
2024-05-11 12:02:45 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "appservice"{
   resource_group_name=var.resource_group_name
   location=var.location
   app_service_name= var.app_service_name
-  app_service_plan_id=module.appserviceplan.id
+  app_service_plan_id=module.appserviceplan.appserviceplanid
   user_assigned_identity=module.umi.id
 }
 
