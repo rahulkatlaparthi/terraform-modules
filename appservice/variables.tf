17980415a0a7d6631ed820aebffddbacc6bfa8e3
Uday kumar <udayjothula@gmail.com>
2024-05-11 10:28:39 +0000

    Updated variables.tf

--- appservice/variables.tf
+++ appservice/variables.tf
@@ -9,3 +9,10 @@ variable "location" {
 
   type        = string
 }
+
+variable "app_service_name" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
