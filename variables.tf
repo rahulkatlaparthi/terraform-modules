6d083db02ea7975d3e26a1a44362ead121e3a3c1
Uday kumar <udayjothula@gmail.com>
2024-05-11 10:35:41 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -38,6 +38,11 @@ variable "size" {
   default     = "S1"
   type        = string
 }
+variable "app_service_name" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+    default     = "appsericeahura"
+  type        = string
+}
 
 
 
