ce87d10805011e0b0e07ddb8022b48783c84bda2
Uday kumar <udayjothula@gmail.com>
2024-05-11 06:25:17 +0000

    Updated storage.tf

--- Storage/storage.tf
+++ Storage/storage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "example" {
   name                     = "storageaccountname"
   resource_group_name      = var.resource_group_name
-  location                 = azurerm_resource_group.example.location
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
