c913bca8408c6d07a3dc86b8daac9e988ef99e19
Uday kumar <udayjothula@gmail.com>
2024-05-11 10:09:45 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
      resource_group_name  = "example-resourcessssesSSSs"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
      storage_account_name = "rahkattak"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
      container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+     key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
    }
 }
 
