b290e331fb05fe1dd4f9eeac232e23cb83e4388d
Uday kumar <udayjothula@gmail.com>
2024-05-11 10:05:17 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ terraform {
       version = "=3.89.0"
     }
   }
-//   backend "azurerm" {
-//     resource_group_name  = "dev"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-//     storage_account_name = "samplest56h"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-//     container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-//     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-//   }
+   backend "azurerm" {
+     resource_group_name  = "example-resourcessssesSSSs"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+     storage_account_name = "rahkattak"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+     container_name       = "sample"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+   }
 }
 
 # Configure the Microsoft Azure Provider
