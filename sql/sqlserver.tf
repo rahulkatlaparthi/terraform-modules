a2974ddb4d7c756372299c155fcabcba14036a47
Uday kumar <udayjothula@gmail.com>
2024-05-12 16:21:20 +0000

    Updated sqlserver.tf

--- sql/sqlserver.tf
+++ sql/sqlserver.tf
@@ -2,7 +2,7 @@ resource "azurerm_sql_server" "example" {
   name                         = var.name
   resource_group_name          = var.resource_group_name
   location                     = var.location
-  version                      = var.version
+  version                      = "12.0"
   administrator_login          = var.administrator_login
   administrator_login_password = var.administrator_login_password
 
