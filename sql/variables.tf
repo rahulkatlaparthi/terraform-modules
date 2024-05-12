a96747501db187127afc3f930fdba06f2df0af3d
Uday kumar <udayjothula@gmail.com>
2024-05-12 16:25:11 +0000

    Updated variables.tf

--- sql/variables.tf
+++ sql/variables.tf
@@ -30,11 +30,7 @@ variable "administrator_login" {
   type        = string
 }
 
-variable "administrator_login" {
-  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
 
-  type        = string
-}
 
 variable "administrator_login_password" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
