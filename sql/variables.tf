95cf9de380268205b46450b6fd8297aeb60e57b2
Uday kumar <udayjothula@gmail.com>
2024-05-12 15:45:48 +0000

    Added variables.tf

---
+++ sql/variables.tf
@@ -0,0 +1,53 @@
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "name" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "version" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "administrator_login" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "administrator_login" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "administrator_login_password" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
+
+variable "environment" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+
+  type        = string
+}
