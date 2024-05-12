4c2ffafeed2a5aa3e9da905c636b9d67db139282
Uday kumar <udayjothula@gmail.com>
2024-05-12 15:50:24 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -49,6 +49,37 @@ variable "user_assigned_identity" {
 
   type        = list(string)
 }
+variable "name" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default="sqlnamerah78"
+  type        = string
+}
+
+variable "version" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default="12"
+  type        = string
+}
+
+
+
+variable "administrator_login" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default="rahullogin"
+  type        = string
+}
+
+variable "administrator_login_password" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default="Rahul@69"
+  type        = string
+}
+
+variable "environment" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default="dev"
+  type        = string
+}
 
 
 
