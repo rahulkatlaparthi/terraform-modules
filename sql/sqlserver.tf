106f7f9e40d391ca39c6f1c85b33ede89e0019b9
Uday kumar <udayjothula@gmail.com>
2024-05-12 15:31:31 +0000

    Added sqlserver.tf

---
+++ sql/sqlserver.tf
@@ -0,0 +1,12 @@
+resource "azurerm_sql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "mradministrator"
+  administrator_login_password = "thisIsDog11"
+
+  tags = {
+    environment = "production"
+  }
+}
