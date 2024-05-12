4cc36a0418caccac64bf500358b53a68183b92ac
Uday kumar <udayjothula@gmail.com>
2024-05-12 15:46:08 +0000

    Updated variables.tf

--- sql/variables.tf
+++ sql/variables.tf
@@ -10,11 +10,7 @@ variable "location" {
   type        = string
 }
 
-variable "location" {
-  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
 
-  type        = string
-}
 
 variable "name" {
   description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
